74217fd314c30c685ae66128e692512cf7781a6f
Administrator <Administrator@MSDN-SPECIAL>
2015-05-20 12:02:18 +0900

    05.20..

--- jse/src/oop01_syntax/CardVO.java
+++ jse/src/oop01_syntax/CardVO.java
@@ -7,19 +7,30 @@ public class CardVO {
 	static int height=300; // 카드의 높이
 
 	public CardVO(String kind,int number) {
-		System.out.println("내 카드는 "+ kind + " 이고"
-				+" 숫자는 "+ number +" 이다.");
+		this.kind = kind;
+		this.number = number;
 	}
-	public void CardVO1(String kind,int number) {
-		System.out.println("당신 카드는 "+ kind + " 이고"
-				+" 숫자는 "+ number +" 이다.");
+	public String getkind(){
+		return kind;
+	}
+	
+	public void setkind(String kind){
+			
+	}
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
 	}
 	
 	
-	public void myCard(String kind,int number){
-		System.out.println("당신 카드의"+"숫자"+"는"+ number+"이고"
-				+ "내 카드"+ "숫자"+"는"+ kind + "이므로"
-				+ "당신이 이겼습니다.");
-	}
+	
 
 }
